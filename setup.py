d0b34248d27ac9f419abe995e2468a96e8cd3dd2
Peter Linss <peter@linss.com>
2020-02-02 13:06:37 -0800

    Fix release info

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
-"""Create pypi package."""
+"""Define PyPI package."""
 
 import setuptools
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
 
 setuptools.setup(
     name='sem_ver',
     version='0.0.0',  # version will get replaced by git version tag - do not edit
     author='Peter Linss',
-    author_email='peter@linss.com',
-    description='Semantic version handler',
+    author_email='pypi@linss.com',
+    description='Semantic Version Processor',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/plinss/sem_ver/',
-    license="XXX",
 
     packages=['sem_ver'],
     package_data={'sem_ver': ['py.typed']},
 
     install_requires=[
-        # include any dependency packages here
     ],
     extras_require={
         'dev': ['mypy',
                 'flake8', 'flake8-import-order', 'flake8-annotations', 'flake8-type-annotations', 'flake8-docstrings',
                 'pep8-naming'],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires='>=3.6'
 )
